8a611c13cdbdb9f87dee89abd57552a5ab833490
Ivan Sanchez <s4nchez@gmail.com>
2012-08-25 20:46:49 +0100

    Reverting 41770b0 because apparently sessions are not working using rackup + Sinatra::Base

--- server/src/web.rb
+++ server/src/web.rb
@@ -1,4 +1,4 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/config_file'
 require 'json'
 
@@ -6,80 +6,75 @@ require_relative 'engine'
 require_relative 'game_state'
 require_relative 'messaging'
 
+set :public_folder, '../client'
 
-module CardGames
-  class Web < Sinatra::Base
+enable :sessions
+set :logging, true
 
-    set :public_folder, '../client'
-    set :sessions, true
-    set :logging, true
-    # required for keep_open
-    set :server, :thin
+messaging = CardGames::Messaging.new
+state = CardGames::GameState.new
+engine = CardGames::Engine.new(messaging, state)
 
-    messaging = CardGames::Messaging.new
-    state = CardGames::GameState.new
-    engine = CardGames::Engine.new(messaging, state)
+def player_session
+  session[:player] = (0...8).map{65.+(rand(25)).chr}.join unless session.has_key? :player
+  session[:player]
+end
 
-    def player_session
-      session[:player] = (0...8).map{65.+(rand(25)).chr}.join unless session.has_key? :player
-      session[:player]
-    end
+get '/' do
+  redirect '/index.html'
+end
 
-    get '/' do
-      redirect '/index.html'
-    end
+get '/command/:command' do
+  details = []
+  details = params["details"].split(",") if params.has_key? "details"
+  engine.process_command(player_session, params[:command], *details)
+  JSON({:result => 'ok'})
+end
 
-    get '/command/:command' do
-      details = []
-      details = params["details"].split(",") if params.has_key? "details"
-      engine.process_command(player_session, params[:command], *details)
-      JSON({:result => 'ok'})
-    end
+post '/command/group' do
+  logger.info { "/command/group received #{params}" }
 
-    post '/command/group' do
-      logger.info { "/command/group received #{params}" }
+  source_group_id = params[:source_group_id]
+  card_idx = params[:card_idx].to_i
+  position = params[:position].map {|n| n.to_i}
 
-      source_group_id = params[:source_group_id]
-      card_idx = params[:card_idx].to_i
-      position = params[:position].map {|n| n.to_i}
+  new_group_id = engine.create_group(player_session, source_group_id, card_idx, position)
+  JSON({:newGroupId => new_group_id})
+end
 
-      new_group_id = engine.create_group(player_session, source_group_id, card_idx, position)
-      JSON({:newGroupId => new_group_id})
-    end
+post '/command/movecard' do
+  logger.info { "/command/movecard received #{params}" }
 
-    post '/command/movecard' do
-      logger.info { "/command/movecard received #{params}" }
+  source_group_id = params[:source_group_id]
+  target_group_id = params[:target_group_id]
+  source_group_card_idx = params[:source_group_card_idx].to_i
+  target_group_card_idx = params[:target_group_card_idx].to_i
 
-      source_group_id = params[:source_group_id]
-      target_group_id = params[:target_group_id]
-      source_group_card_idx = params[:source_group_card_idx].to_i
-      target_group_card_idx = params[:target_group_card_idx].to_i
+  engine.move_card(player_session, source_group_id, source_group_card_idx, target_group_id, target_group_card_idx)
+  JSON({:result => 'ok'})
+end
 
-      engine.move_card(player_session, source_group_id, source_group_card_idx, target_group_id, target_group_card_idx)
-      JSON({:result => 'ok'})
-    end
+get '/query' do
+  JSON(messaging.query(player_session))
+end
 
-    get '/query' do
-      JSON(messaging.query(player_session))
-    end
+get '/current-state' do
+  state.player_active(player_session)
+  result = state.current_state(player_session)
+  logger.info { "#{result}" }
+  JSON(result)
+end
 
-    get '/current-state' do
-      state.player_active(player_session)
-      result = state.current_state(player_session)
-      logger.info { "#{result}" }
-      JSON(result)
-    end
+# required for keep_open
+set :server, :thin
 
-    get '/message-stream', provides: 'text/event-stream' do
-      stream :keep_open do |out|
-        messaging.register_client(player_session, lambda { |messages|
-          out << "event: messages\n"
-          out << "data: " + JSON(messages) + "\n\n"
-          logger.info("Sent message to " + player_session)
-        })
-        out.callback { messaging.unregister_client(player_session) }
-      end
-    end
-
+get '/message-stream', provides: 'text/event-stream' do
+ stream :keep_open do |out|
+    messaging.register_client(player_session, lambda { |messages|
+      out << "event: messages\n"  
+      out << "data: " + JSON(messages) + "\n\n"
+      logger.info("Sent message to " + player_session)
+    })
+    out.callback { messaging.unregister_client(player_session) }
   end
 end
